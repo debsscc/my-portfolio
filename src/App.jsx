d330e82ecd85419731c8b4bce965a01bb68da75c
Debora Carvalho <dbndy3@gmail.com>
2025-07-28 22:58:59 -0300

    ✨feat: Analytics app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
@@ -19,6 +20,7 @@ const App = () => {
           <StarsCanvas />
         </div>
       </div>
+      <Analytics />
     </BrowserRouter>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Analytics } from "@vercel/analytics/next"
 
 import App from "./App";
 import "./index.css";
